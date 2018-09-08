da2ed068920b7c50c6b82eb6f0dad08a5944d45d
Maximilian Knapperzbusch <maximilianknapperzbusch@gmail.com>
2018-09-08 11:28:55 +0200

    final fixes

--- api/services/fire_not.py
+++ api/services/fire_not.py
@@ -3,8 +3,9 @@ from pyfcm import FCMNotification
 push_service = FCMNotification(api_key="AAAAK4eM42o:APA91bHMMHAJcTw-iQuK-R3CPMbUrqbfXdDj1DJyPljr7gLGmeH6r6RUwpUcfOcuHnLimtkrqWzkbcTux-uMRRX5Mffx4FX7LI5TVXqQ4nCTUV1n8qWQbWhe-9iY22Pu1BOWxsmLxE4L")
 
 def send_notification(imo, owner, lat, lon, timestamp, vtype, name):
-    registration_id = "ezk9vz373Ps:APA91bHKz5UOGzbcqOuZnTW6ZN-pMFtlJbenqcdANyYYd8qSJGvGs18iB7b1VxgRcU06Q9ez3j3n0bfccMv2Cpb2xapCx1DKxqsTSMJn4ESaagWGGtDHTZZb1CdQl6lzAn1QuDv7qO_P"
-    message_title = "Vessel enters scrapping area"
-    message_body = {"imo": imo, "owner": owner, "lat":lat, "lon": lon, "time": timestamp, "vtype": vtype, "name": name}
+    registration_id = "eDIJvk1E-vI:APA91bGWxJweXGA3wlUshcOTfPQh3AjMnI4I8UbE0m_ecWZADWoP_kqTkVZ9fGAoKtAXgIIadgMpV9L6Hry7otyfA9slj2UEFGpGFV8lON4bYdm6QV5kVFwW-_WAU-W1DkyrEu8xBlei"
+    message_title = "Scrapping Alert"
+    message_body = name + ' entered the Alang scrapping area'
+    data = {"imo": imo, "owner": owner, "lat":lat, "lon": lon, "time": timestamp, "vtype": vtype, "name": name}
     result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title,
-                                               message_body=message_body, click_action="http://localhost:8887/")
+                                               data_message=data, click_action="http://localhost:8887/", message_body=message_body)
